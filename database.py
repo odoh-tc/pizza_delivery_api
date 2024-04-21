45323e6b1c525e553317cfb20120642817d3485d
Tochukwu Odoh <cashearncrypto@gmail.com>
2024-04-21 05:32:59 +0100

    modified the database

--- database.py
+++ database.py
@@ -1,23 +1,3 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, declarative_base
-
-
-# # Define the SQLAlchemy database URL
-# SQLALCHEMY_DATABASE_URL = "sqlite:///database.db"
-
-
-# # Create the SQLAlchemy engine
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-# # Create a sessionmaker to create sessions for interacting with the database
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-# session = SessionLocal()
-
-# # Create a base class for your SQLAlchemy models
-# Base = declarative_base()
-
 import os
 from typing import Annotated
 from dotenv import load_dotenv
